e080fc4872ed86233758d350daaf6d8aab68a472
MalinJ123 <malin--jonsson@hotmail.com>
2023-02-14 08:30:21 +0100

    you clicked

---
+++ html/script.js
@@ -0,0 +1,10 @@
+let button = document.querySelector('#my-button')
+// let body = document.querySelector('body')
+button.addEventListener('click', onClick)
+
+
+function onClick(){
+	console.log(' Du klickade')
+}
+
+
